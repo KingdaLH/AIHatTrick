081157c24c844cb749dda0af2272fdf027d27aad
David Poels <326668@student.mboutrecht.nl>
2020-12-18 19:44:35 +0100

    Added the files

---
+++ Assets/Hat Trick - The Catch Game/Scripts/HT_AI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class HT_AI : MonoBehaviour
+{
+    private HT_GameController GC;
+    private Vector3 targetPos;
+    private float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GC = GameObject.FindObjectOfType<HT_GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GC.list.Count != 0)
+        {
+            if (GC.list[0].tag == "Ball")
+            {
+                targetPos = new Vector3(GC.list[0].transform.position.x, 0, 0);
+            }
+            else
+            {
+                if (GC.list[0].transform.position.x > 0)
+                {
+                    targetPos = new Vector3(-2, 0, 0);
+                }
+                else
+                {
+                    targetPos = new Vector3(7, 0, 0);
+                }
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * moveSpeed);
+    }
+}
---
+++ Assets/Hat Trick - The Catch Game/Scripts/HT_DestroyOnContact.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+
+public class HT_DestroyOnContact : MonoBehaviour
+{
+	private HT_GameController GM;
+
+	private void Start()
+	{
+		GM = GameObject.FindObjectOfType<HT_GameController>();
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		GM.list.Remove(other.gameObject);
+		Destroy (other.gameObject);
+	}
+}
